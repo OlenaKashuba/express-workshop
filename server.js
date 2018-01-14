42196c15b484ed2309e49071c8686d50322a404d
OlenaKashuba <amely.jazz@gmail.com>
2018-01-14 13:39:25 +0000

    Add endpoints

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
 
+
+app.get('/', (req,res) => res.send(`This is main page`));
+app.get('/cyf', (req,res) => res.send(`This is CYF page`));
+app.get('/nodegirls', (req,res) => res.send(`This is NodeGirls page`));
+
 app.listen(3000, () => console.log(`Server is listening on port 3000! Ready to accept requests!`));
