3acc0a3e9ffed0fa0e9f178889488ae54f1ad348
OlenaKashuba <amely.jazz@gmail.com>
2018-01-14 14:13:29 +0000

    Send blogpost to the server

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const formidable = require('express-formidable');
 const app = express();
 
 
-app.get('/', (req,res) => res.send(`This is main page`));
-app.get('/cyf', (req,res) => res.send(`This is CYF page`));
-app.get('/nodegirls', (req,res) => res.send(`This is NodeGirls page`));
+
+// app.get('/', (req,res) => res.send(`This is main page`));
+// app.get('/cyf', (req,res) => res.send(`This is CYF page`));
+// app.get('/nodegirls', (req,res) => res.send(`This is NodeGirls page`));
+
+app.use(express.static('public'));
+app.use(formidable());
+
+app.post('/create-post', (req,res) => console.log(req.fields));
 
 app.listen(3000, () => console.log(`Server is listening on port 3000! Ready to accept requests!`));
