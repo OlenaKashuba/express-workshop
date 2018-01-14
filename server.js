5e8c9d9d323f2798e8a20ad7f2b9d06de5f7f0ea
OlenaKashuba <amely.jazz@gmail.com>
2018-01-14 17:16:54 +0000

    Make posts visible in html

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const formidable = require('express-formidable');
+const fs = require('fs');
+
 const app = express();
 
 
@@ -8,9 +10,38 @@ const app = express();
 // app.get('/cyf', (req,res) => res.send(`This is CYF page`));
 // app.get('/nodegirls', (req,res) => res.send(`This is NodeGirls page`));
 
+
+
 app.use(express.static('public'));
 app.use(formidable());
 
-app.post('/create-post', (req,res) => console.log(req.fields));
+app.post('/create-post', (req,res) => {
+	fs.readFile(__dirname + '/data/posts.json', (error,file) => {
+		let parsedFile = JSON.parse(file);
+		parsedFile[Date.now()] = req.fields.blogpost;
+		fs.writeFile(__dirname + '/data/posts.json', JSON.stringify(parsedFile), error => {});
+	});
+});
+
+app.get('/get-posts', (req,res) => {
+	// fs.readFile(__dirname + '/data/posts.json', (err, data) => {
+ //  		if (err) throw err;
+ // 		 res.send(JSON.parse(data));
+	// });
+	res.sendFile(__dirname + '/data/posts.json',  (err) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('data');
+		}
+	});
+});
+
+
+// fs.readFile(__dirname + '/data/posts.json', (error,file) => {
+// 	let parsedFIle = JSON.parse(file);
+// 	console.log(parsedFIle);
+// });
+
 
 app.listen(3000, () => console.log(`Server is listening on port 3000! Ready to accept requests!`));
